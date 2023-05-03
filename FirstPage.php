8ef0a43e449a6fefce450ad1cb47341ed4eecfc6
Shreetej Pandit <62033741+ShreetejPandit@users.noreply.github.com>
2023-05-03 10:02:02 +0530

    first commit

---
+++ FirstPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Page title</title>
+</head>
+<body>
+    
+    <h1>This is heading</h1>
+
+    <p>This is Paragraph.</p>
+
+    <P>this is another Paragraph</P>
+
+</body>
+</html>
